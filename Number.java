55b11deb12cd509051edf19f130de630fa291545
sknzahra <61772310+sknzahra@users.noreply.github.com>
2021-01-19 00:10:16 +0700

    Update Number.java

--- Number.java
+++ Number.java
@@ -1,8 +1,20 @@
 public class Number {
  public static void main(String args[]) {
- System.out.println("Hello World");
- for (int i=0; i<10; i++){
- System.out.println(i);
- }
+ int input = 100;
+        System.out.println("Bilangan Prima 1 hingga 100 adalah : ");
+        for(int i=2; i<input; i++) {
+            boolean isPrima = true;
+            
+            for (int j = 2; j < i; j++) {
+                if(i%j==0){
+                    isPrima = false;
+                    break;
+                    
+                }
+            }
+            if(isPrima==true){
+                System.out.print(i+",");
+            }
+        }
  }
 }
